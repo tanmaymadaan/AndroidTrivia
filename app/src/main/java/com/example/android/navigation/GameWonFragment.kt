cb836fb05fa30d4184e8a7fec7da3c6e40de3460
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-18 14:58:17 +0530

    Using ShareCompat for sharing intent

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -42,17 +43,17 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            menu?.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
     private fun getShareIntent() : Intent {
         var args = GameWonFragmentArgs.fromBundle(requireArguments())
-        val shareIntent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-                .putExtra(Intent.EXTRA_TEXT,
-                getString(R.string.share_success_text, args.numCorrect,
-                        args.numQuestions))
-
-        return shareIntent
+        return ShareCompat.IntentBuilder.from(requireActivity())
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
     }
 
     private fun shareSuccess() {
@@ -66,3 +67,4 @@ class GameWonFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 }
+
